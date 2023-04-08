902ab9c0b8385ade8e7e5f3f989cb87e8c15a136
hoangtv090103 <hoangtv090103@gmail.com>
2023-04-09 01:44:07 +0700

    Array in Python

---
+++ Python/array.py
@@ -0,0 +1,31 @@
+# Experiments using Python arrays and vectors
+
+
+def array_test():
+    ar = [3, 2, 4, 5]
+
+    print(ar[0]) # First element of the array
+    ar.pop()
+
+    ar.append(6)
+
+    print(ar)
+    print("Index of 4: ", ar.index(4))  # index of given value
+
+    ar.remove(4)  # remove the first occurence of item with given value
+    print("Removed 4: ", ar)
+
+    ar.reverse()
+    print("reversed: ", ar)
+    print("sorted return: ", sorted(ar))
+
+    ar.sort()
+    print("sorted in place: ", ar)
+
+
+def main():
+    array_test()
+
+
+if __name__ == "__main__":
+    main()
